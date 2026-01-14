3a35ac1f69e1fcfb670b1f884f393f77299bcbb0
Kingjaybail <Kingajaybail@gmail.com>
2026-01-14 11:43:06 -0600

    made navname link

--- temp-frontend/src/components/Navbar/navbar.jsx
+++ temp-frontend/src/components/Navbar/navbar.jsx
@@ -1,7 +1,16 @@
 import "./navbar.scss";
+import { useEffect, useState } from "react";
+import { fetchUsers } from "../../util/router";
 
 export default function Navbar() {
   const user = localStorage.getItem("user");
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers()
+      .then(setUsers)
+      .catch(() => setError("Failed to load users"));
+  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem("user");
@@ -20,18 +29,23 @@ export default function Navbar() {
       </header>
 
       <nav className="top-nav">
-        <a href="/">Home</a>
-        <a href="/create">Create Article</a>
-        <a href="/articles">Articles</a>
+        <a className="nav-button" href="/">Home</a>
+        <a className="nav-button" href="/create">Create Article</a>
+        <a className="nav-button" href="/articles">Articles</a>
 
         <div className="nav-right">
           {user ? (
             <>
-              <span className="nav-user">Logged in as: <strong>{user}</strong></span>
-              <a href="#" onClick={handleLogout}>Logout</a>
+              <span className="nav-user">
+                  Logged in as:{" "}
+                  <a href={`/users/${user}`} className="nav-user-link">
+                    <strong>{user}</strong>
+                  </a>
+              </span>
+              <a href="#" className="nav-button" onClick={handleLogout}>Logout</a>
             </>
           ) : (
-            <a href="/login">Login</a>
+            <a href="/login" className="nav-button">Login</a>
           )}
         </div>
       </nav>
