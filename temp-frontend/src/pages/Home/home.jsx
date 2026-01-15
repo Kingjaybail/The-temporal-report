e28327368f7992e187b98d879982a62a81b50e39
Kingjaybail <Kingajaybail@gmail.com>
2026-01-15 00:55:50 -0600

    added footnote easter egg

--- temp-frontend/src/pages/Home/home.jsx
+++ temp-frontend/src/pages/Home/home.jsx
@@ -2,6 +2,19 @@ import "./home.scss"
 import Navbar from "../../components/Navbar/navbar.jsx";
 
 export default function Home() {
+    const showRareFooter = Math.floor(Math.random() * 25) === 0;
+    const normalFooter = (
+      <>
+        The Temporal Report 2003–2005 <br />
+        Editors: note as of December 22nd 2025 we are no longer partnered with the Epstein Foundation of youths in congress
+      </>
+    );
+
+    const rareFooter = (
+      <>
+        Hint: A note exists beneath my feat, a trail it leads, start at the beginning but gaze upon the end <br/>
+      </>
+    );
 
   return (
     <div className="page">
@@ -38,10 +51,9 @@ export default function Home() {
         </main>
       </div>
 
-      <footer className="footer">
-        The Temporal Report 2003–2005 <br></br>
-        Editors: note as of December 22nd 2025 we are no longer partnered with the Epstein Foundation of youths in congress
-      </footer>
+        <footer className="footer">
+          {showRareFooter ? rareFooter : normalFooter}
+        </footer>
     </div>
   );
 }
